276064e273031e4525f5bd5200e471647131351d
Shadab <shadab19it@gmail.com>
2020-08-08 00:19:11 +0530

    title set in center

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 function index() {
   return (
     <div>
-      <h1>this is the first page</h1>
+      <h1>This is the first Next.js app that I deploy</h1>
       <Link href='Details'>
         <a>Details</a>
       </Link>
@@ -12,15 +12,18 @@ function index() {
       <Link as='/car/shadab' href='[vechels]/[person]'>
         <a>dynamic</a>
       </Link>
+      <style jsx>
+        {`
+          h1 {
+            text-align: center;
+          }
+          a {
+            padding: 20px;
+          }
+        `}
+      </style>
     </div>
   );
 }
-
-<style
-  jsx={`
-  a{
-    padding:20px;
-  }
-`}></style>;
 
 export default index;
